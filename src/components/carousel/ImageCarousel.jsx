5ddb0801547c33c63e06af9aad6b81c87909a0ae
lazingbird <lazingbird@protonmail.com>
2024-01-17 19:54:30 -0300

    update image carousel

--- src/components/carousel/ImageCarousel.jsx
+++ src/components/carousel/ImageCarousel.jsx
@@ -6,8 +6,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
   faChevronRight,
+  faCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import imageCarouselData from "../../data/imageCarouselData";
+
+const Dots = ({ imageIndex, setImageIndex, isLoaded, setIsLoaded }) => {
+  const dots = [0, 1, 2, 3];
+
+  const imageTransition = () => {
+    setIsLoaded(false);
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 100);
+  };
+
+  const handleClickDot = (dot) => {
+    imageTransition();
+    setImageIndex(dot);
+  };
+
+  return (
+    <div className="flex gap-1">
+      {dots.map((dot) => (
+        <span
+          key={dot}
+          onClick={() => handleClickDot(dot)}
+          className={`mt-4 cursor-pointer select-none text-xs transition delay-100 duration-200 ease-in-out hover:opacity-70 ${isLoaded ? "opacity-100" : "opacity-50"} ${imageIndex === dot ? "text-purple-900" : "text-slate-300 "}`}
+        >
+          ⬤
+        </span>
+      ))}
+    </div>
+  );
+};
 
 const ImageCarousel = () => {
   const [imageIndex, setImageIndex] = useState(0);
@@ -36,26 +67,34 @@ const ImageCarousel = () => {
     setImageIndex(imageIndex - 1);
   };
   return (
-    <section className="display: flex items-center justify-center pt-28">
-      <FontAwesomeIcon
-        className="size-12 cursor-pointer select-none hover:text-purple-900"
-        icon={faChevronLeft}
-        onClick={handlePrev}
-      ></FontAwesomeIcon>
-      {imageCarouselData.map((data, index) => (
-        <SlideContainer
-          key={index}
-          data={data}
-          index={index}
-          imageIndex={imageIndex}
-          isLoaded={isLoaded}
-        ></SlideContainer>
-      ))}
-      <FontAwesomeIcon
-        onClick={handleNext}
-        className="size-12 cursor-pointer select-none hover:text-purple-900"
-        icon={faChevronRight}
-      ></FontAwesomeIcon>
+    <section className="flex  flex-col items-center justify-center pt-28">
+      <div className="flex items-center ">
+        <FontAwesomeIcon
+          className="size-12 cursor-pointer select-none hover:text-purple-900"
+          icon={faChevronLeft}
+          onClick={handlePrev}
+        ></FontAwesomeIcon>
+        {imageCarouselData.map((data, index) => (
+          <SlideContainer
+            key={index}
+            data={data}
+            index={index}
+            imageIndex={imageIndex}
+            isLoaded={isLoaded}
+          ></SlideContainer>
+        ))}
+        <FontAwesomeIcon
+          onClick={handleNext}
+          className="size-12 cursor-pointer select-none hover:text-purple-900"
+          icon={faChevronRight}
+        ></FontAwesomeIcon>
+      </div>
+      <Dots
+        imageIndex={imageIndex}
+        setImageIndex={setImageIndex}
+        isLoaded={isLoaded}
+        setIsLoaded={setIsLoaded}
+      ></Dots>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        "3xl":
-          "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+        "3xl": "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
       },
       fontFamily: {
         quicksand: ["Quicksand", "sans-serif"],
