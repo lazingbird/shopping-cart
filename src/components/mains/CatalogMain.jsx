34186c1c214df17c4fc3936633cbe65cc0b5c3a9
lazingbird <lazingbird@protonmail.com>
2024-01-25 16:52:19 -0300

    add pagination to catalog

--- src/components/mains/CatalogMain.jsx
+++ src/components/mains/CatalogMain.jsx
@@ -6,13 +6,36 @@ import { useState, useEffect } from "react";
 
 import CatalogProducts from "./CatalogProducts";
 
+const ShowPage = ({ page, products }) => {
+  let productList = products.slice(page * 12, page * 12 + 12);
+  return (
+    <>
+      {productList.map((product) => (
+        <CatalogProducts key={uuidv4()} data={product}></CatalogProducts>
+      ))}
+    </>
+  );
+};
+
 const CatalogMain = () => {
   const [products, setProducts] = useState(null);
+  const [pages, setPages] = useState([1]);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const itemPerPage = 12;
 
   useEffect(() => {
     const fetchData = async () => {
+      let nextPage = 2;
+      let tempPages = [1];
       const result = await Promise.all(
-        fakeProducts.allFakeProducts.map(async (item) => {
+        fakeProducts.allFakeProducts.map(async (item, index) => {
+          console.log((index + 1) % 12);
+          if ((index + 1) % 12 === 0) {
+            tempPages.push(nextPage);
+            nextPage = nextPage + 1;
+          }
           let request = await games.getById(item.rawg_id);
           return {
             ...item,
@@ -21,6 +44,8 @@ const CatalogMain = () => {
         }),
       );
       setProducts(result);
+      setTotalProducts(result.length);
+      setPages(tempPages);
     };
     fetchData();
   }, []);
@@ -30,12 +55,26 @@ const CatalogMain = () => {
   }
   return (
     <section className="mt-10 flex flex-col items-center">
-      <h2 className="text-roxoMuitoJogo mb-6 text-3xl font-bold">
+      <h2 className="mb-6 text-3xl font-bold text-roxoMuitoJogo">
         Todos Produtos
       </h2>
-      <div className="catalog-grid grid w-8/12">
-        {products.map((product) => (
+      <div className="catalog-grid grid w-8/12 ">
+        <ShowPage page={currentPage} products={products}></ShowPage>
+        {/* {products.slice(0, itemPerPage).map((product) => (
           <CatalogProducts key={uuidv4()} data={product} />
+        ))} */}
+      </div>
+      <div className="mt-5 flex gap-5">
+        {pages.map((pageNumber) => (
+          <button
+            style={{
+              fontWeight: pageNumber - 1 === currentPage ? "bold" : "thin",
+            }}
+            onClick={() => setCurrentPage(pageNumber - 1)}
+            key={uuidv4()}
+          >
+            {pageNumber}
+          </button>
         ))}
       </div>
     </section>
